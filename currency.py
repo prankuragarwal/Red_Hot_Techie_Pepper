2b3c92d7492cec38a2fc1ccef2049e1069ff6a5b
prankuragarwal <prankur.agarwal18@gmail.com>
2018-03-17 03:13:14 +0530

    Currency Class

--- currency.py
+++ currency.py
@@ -1,19 +1,14 @@
 import requests
+import json
 
-a = input('Enter currency to convert from?')
-a = a.upper()
+#convert eur to inr 
+class curr(object):
+	def __init__(self):
+		self.url = 'https://currency-api.appspot.com/api/'
 
-b = input('Enter currency to convert to?')
-b = b.upper()
-
-c = float(input('Enter value to convert?'))
-
-url = ('https://currency-api.appspot.com/api/%s/%s.json') % (a, b)
-#print (url)
-
-r = requests.get(url)
-print (r.json()['rate'])
-
-print (c*float(r.json()['rate']))
-
-
+	def curfun(self, content):
+		fr = content[2]
+		to = content[4]
+		#self.url = self.url + fr + '/' + to + '.json'
+		self.r = requests.get(self.url+ fr + '/' + to + '.json')
+		return float(self.r.json()['rate'])
